a781f69e85fbd36d33fe262d50f81abecc656f3a
Mono-ceros <137870698+Mono-ceros@users.noreply.github.com>
2023-08-17 10:03:04 +0900

    1.1(Complete)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -34,72 +34,81 @@ public class GameController : MonoBehaviour
     public Text highScoreText;
     int highScore = 0;
 
-    //버스트랑 블랙잭만 건들이면 끝날듯
     private void Start()
     {
         highScore = PlayerPrefs.GetInt("HighScore");
         highScoreText.text = highScore.ToString();
     }
 
-    //이거 코루틴들어가있음
-    public void Hit()
+    #region Button
+    public void HitPlay()
     {
-        player.Push(deck.Pop());
-        if (player.HandValue() > 21)
-        {
-            PlayButtonOff();
-            winnerText.text = "버스트";
-            winnerText.text = "패배";
-            resultChipText.text = "칩 +0";
-            HighScoreUpdate();
-
-            if (currentChip <= 0)
-            {
-                winnerText.text = "파산";
-                ResultOn();
-                PlayAgainButton.SetActive(false);
-                yield return new WaitForSeconds(2f);
-                ResultOff();
-                PlayAgainButton.SetActive(true);
-                gameOverPanel.SetActive(true);
-            }
-            else
-            {
-                ResultOn();
-            }
-        }
+        StartCoroutine(Hit());
     }
 
+    IEnumerator Hit()
+    {
+        hitButton.interactable = false;
+        stickButton.interactable = false;
+        player.Push(deck.Pop());
+        yield return new WaitForSeconds(0.5f);
+        Burst();
+        hitButton.interactable = true;
+        stickButton.interactable = true;
+    }
 
-    public void Stick()
+    public void Stand()
     {
         hitButton.interactable = false;
         stickButton.interactable = false;
         StartCoroutine(DealersTurn());
     }
 
-    void HighScoreUpdate()
+    public void Bet()
     {
-        if (currentChip > PlayerPrefs.GetInt("HighScore"))
+        inputFieldText = inputField.text;
+        int.TryParse(inputFieldText, out bettingChip);
+        if(bettingChip > currentChip || bettingChip <= 0)
         {
-            PlayerPrefs.SetInt("HighScore", currentChip);
-            highScore = PlayerPrefs.GetInt("HighScore");
-            highScoreText.text = highScore.ToString();
+            logText.text = "올바른 배팅이 아닙니다.";
+            return;
+        }
+        betGroup.SetActive(false);
+        currentChip -= bettingChip;
+        currentChipText.text = currentChip.ToString();
+        StartGame();
+    }
+
+    public void NewGame()
+    {
+        gameOverPanel.SetActive(false);
+        currentChip = 100;
+        PlayAgain();
+    }
+
+    public void EndApp()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+
+    #region Result
+    void Burst()
+    {
+        if (player.HandValue() > 21)
+        {
+            PlayButtonOff();
+            winnerText.text = "버스트";
+            resultChipText.text = "칩 +0";
+            HighScoreUpdate();
+
+            StartCoroutine(IsGameOver());
         }
     }
 
-    void StartGame()
+    void BackJack()
     {
-        bettingChipText.text = bettingChip.ToString();
-        currentChipText.text = currentChip.ToString();
-        PlayButtonOn();
-
-        for (int i = 0; i < 2; i++)
-        {
-            player.Push(deck.Pop());
-            HitDealer();
-        }
-
         if (player.HandValue() == 21)
         {
             PlayButtonOff();
@@ -112,6 +121,48 @@ public class GameController : MonoBehaviour
         }
     }
 
+    void OutCome()
+    {
+        if (dealer.HandValue() == player.HandValue())
+        {
+            winnerText.text = "무승부";
+            resultChipText.text = "칩 +" + bettingChip;
+            currentChip += bettingChip;
+        }
+        else if (dealer.HandValue() >= player.HandValue() && dealer.HandValue() <= 21)
+        {
+            winnerText.text = "패배";
+            resultChipText.text = "칩 +0";
+        }
+        else if (dealer.HandValue() > 21 || (player.HandValue() <= 21 && player.HandValue() > dealer.HandValue()))
+        {
+            winnerText.text = "승리";
+            resultChipText.text = "칩 +" + (bettingChip * 2);
+            currentChip += bettingChip * 2;
+        }
+    }
+
+    IEnumerator IsGameOver()
+    {
+        if (currentChip <= 0)
+        {
+            winnerText.text = "파산";
+            ResultOn();
+            PlayAgainButton.SetActive(false);
+            yield return new WaitForSeconds(1f);
+            ResultOff();
+            PlayAgainButton.SetActive(true);
+            gameOverPanel.SetActive(true);
+        }
+        else
+        {
+            ResultOn();
+        }
+    }
+
+    #endregion
+
+    #region OnOff
     void PlayButtonOn()
     {
         playButtonPanel.SetActive(true);
@@ -133,53 +184,9 @@ public class GameController : MonoBehaviour
     {
         resultGroup.SetActive(false);
     }
+    #endregion
 
-    public void PlayAgain()
-    {
-        bettingChipText.text = "0";
-        currentChipText.text = currentChip.ToString();
-        player.GetComponent<CardStackView>().Clear();
-        dealer.GetComponent<CardStackView>().Clear();
-        deck.GetComponent<CardStackView>().Clear();
-        deck.CreateDeck();
-        ResultOff();
-        betGroup.SetActive(true);
-
-        hitButton.interactable = true;
-        stickButton.interactable = true;
-
-        dealersFirstCard = -1;
-    }
-
-    public void NewGame()
-    {
-        gameOverPanel.SetActive(false);
-        currentChip = 100;
-        PlayAgain();
-    }
-
-    public void Bet()
-    {
-        inputFieldText = inputField.text;
-        int.TryParse(inputFieldText, out bettingChip);
-        if(bettingChip > currentChip || bettingChip <= 0)
-        {
-            logText.text = "올바른 배팅이 아닙니다.";
-            return;
-        }
-        betGroup.SetActive(false);
-        currentChip -= bettingChip;
-        currentChipText.text = currentChip.ToString();
-        StartGame();
-    }
-
-
-
-    public void EndGame()
-    {
-        Application.Quit();
-    }
-
+    #region Dealer
     void HitDealer()
     {
         int card = deck.Pop();
@@ -206,47 +213,62 @@ public class GameController : MonoBehaviour
         view.ShowCards();
         yield return new WaitForSeconds(1f);
 
+        //딜러 패가 17이상이 될때까지 딜러히트
         while (dealer.HandValue() < 17)
         {
             HitDealer();
             yield return new WaitForSeconds(1f);
-        } 
-
-        if(dealer.HandValue() == player.HandValue())
-        {
-            winnerText.text = "무승부";
-            resultChipText.text = "칩 +" + bettingChip;
-            currentChip += bettingChip;
-        }
-        else if (dealer.HandValue() >= player.HandValue() && dealer.HandValue() <= 21)
-        {
-            winnerText.text = "패배";
-            resultChipText.text = "칩 +0";
-        }
-        else if (dealer.HandValue() > 21 || (player.HandValue() <= 21 && player.HandValue() > dealer.HandValue()))
-        {
-            winnerText.text = "승리";
-            resultChipText.text = "칩 +" + (bettingChip * 2);
-            currentChip += bettingChip * 2;
         }
 
+        OutCome();
         HighScoreUpdate();
-
-        if (currentChip <= 0)
-        {
-            winnerText.text = "파산";
-            ResultOn();
-            PlayAgainButton.SetActive(false);
-            yield return new WaitForSeconds(2f);
-            ResultOff();
-            PlayAgainButton.SetActive(true);
-            gameOverPanel.SetActive(true);
-        }
-        else
-        {
-            ResultOn();
-        }
-        
+        StartCoroutine(IsGameOver());
         yield return new WaitForSeconds(1f);
     }
+
+    #endregion
+
+    void HighScoreUpdate()
+    {
+        if (currentChip > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", currentChip);
+            highScore = PlayerPrefs.GetInt("HighScore");
+            highScoreText.text = highScore.ToString();
+        }
+    }
+
+    void StartGame()
+    {
+        bettingChipText.text = bettingChip.ToString();
+        currentChipText.text = currentChip.ToString();
+        PlayButtonOn();
+
+        for (int i = 0; i < 2; i++)
+        {
+            player.Push(deck.Pop());
+            HitDealer();
+        }
+
+        BackJack();
+        logText.text = "";
+    }
+
+    public void PlayAgain()
+    {
+        bettingChipText.text = "0";
+        currentChipText.text = currentChip.ToString();
+        player.GetComponent<CardStackView>().Clear();
+        dealer.GetComponent<CardStackView>().Clear();
+        deck.GetComponent<CardStackView>().Clear();
+        deck.CreateDeck();
+        ResultOff();
+        betGroup.SetActive(true);
+
+        hitButton.interactable = true;
+        stickButton.interactable = true;
+
+        dealersFirstCard = -1;
+    }
+
 }
